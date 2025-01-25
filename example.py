3bc709bd75c7961894a025833120f5d54cb998f0
Anton Shiryaev <a.shiryaev@cashalot.mcfef.ru>
2025-01-25 17:57:55 +1000

    feat(example): Добавил пример использования
    Добавил пример использования

---
+++ example.py
@@ -0,0 +1,37 @@
+# пример использования MetricEvaluator
+# на датсете: data/snils_dataset_annotations.csv
+# ответах модели: data/snils_MODELFRAMEWORK_Qwen2-VL-2B-Instruct_VQA_answers_20250124_125639.csv
+
+from metric_evaluator.metric_evaluator import MetricEvaluator
+
+if __name__ == "__main__":
+    dataset_annot = "data/snils_dataset_annotations.csv"
+    model_answers = (
+        "data/snils_MODELFRAMEWORK_Qwen2-VL-2B-Instruct_VQA_answers_20250124_125639.csv"
+    )
+
+    metric_eval = MetricEvaluator(dataset_annot, model_answers)
+
+    df_by_id = metric_eval.calculate_metrics_by_id()
+    df_by_id.to_csv(
+        csv_path="workspace/ModelsMetrics/by_id.csv",
+        sep=";",
+        encoding="utf-8-sig",
+        index=False,
+    )
+
+    df_by_doc_type = metric_eval.calculate_metrics_by_doc_type()
+    df_by_doc_type.to_csv(
+        csv_path="workspace/ModelsMetrics/df_by_doc_type.csv",
+        sep=";",
+        encoding="utf-8-sig",
+        index=False,
+    )
+
+    df_general = metric_eval.calculate_metrics_general()
+    df_general.to_csv(
+        csv_path="workspace/ModelsMetrics/df_general.csv",
+        sep=";",
+        encoding="utf-8-sig",
+        index=False,
+    )
