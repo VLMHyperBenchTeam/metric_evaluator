bb46ccea9b3f8c293f91dd00d23fe83539a8bff9
“Anton <a.shiryaev@cashalot.mcfef.ru>
2025-01-29 04:45:43 +1000

    fix(csv path): Передавать название csv файла в качестве параметра
    Передавать название csv файла в качестве параметра

--- example.py
+++ example.py
@@ -12,25 +12,28 @@ if __name__ == "__main__":
 
     metric_eval = MetricEvaluator(dataset_annot, model_answers)
 
+    df_by_id_path_csv = "workspace/ModelsMetrics/by_id.csv"
     df_by_id = metric_eval.calculate_metrics_by_id()
     df_by_id.to_csv(
-        csv_path="workspace/ModelsMetrics/by_id.csv",
+        df_by_id_path_csv,
         sep=";",
         encoding="utf-8-sig",
         index=False,
     )
 
-    df_by_doc_type = metric_eval.calculate_metrics_by_doc_type()
+    df_by_doc_type_path_csv = "workspace/ModelsMetrics/df_by_doc_type.csv"
+    df_by_doc_type = metric_eval.calculate_metrics_by_doc_type(df_by_id)
     df_by_doc_type.to_csv(
-        csv_path="workspace/ModelsMetrics/df_by_doc_type.csv",
+        df_by_doc_type_path_csv,
         sep=";",
         encoding="utf-8-sig",
         index=False,
     )
 
+    df_general_csv_path = "workspace/ModelsMetrics/df_general.csv"
     df_general = metric_eval.calculate_metrics_general()
     df_general.to_csv(
-        csv_path="workspace/ModelsMetrics/df_general.csv",
+        df_general_csv_path,
         sep=";",
         encoding="utf-8-sig",
         index=False,
