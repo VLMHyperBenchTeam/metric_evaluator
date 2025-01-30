6b9a9d4c24d159a8aa673b373bbb07cbd8180df7
“Anton <a.shiryaev@cashalot.mcfef.ru>
2025-01-31 02:28:22 +1000

    feat(example): Добавил пример использования save_function_results
    Добавил пример использования save_function_results

---
+++ example2.py
@@ -0,0 +1,33 @@
+# пример использования MetricEvaluator
+# на датсете: data/snils_dataset_annotations.csv
+# ответах модели: data/snils_MODELFRAMEWORK_Qwen2-VL-2B-Instruct_VQA_answers_20250124_125639.csv
+
+from metric_evaluator.metric_evaluator import MetricEvaluator
+
+if __name__ == "__main__":
+    dataset_annot = "data/snils_dataset_annotations.csv"
+    model_answers = (
+        "data/snils_MODELFRAMEWORK_Qwen2-VL-2B-Instruct_VQA_answers_20250124_125639.csv"
+    )
+
+    metric_eval = MetricEvaluator(dataset_annot, model_answers)
+
+    df_by_id_path_csv = "workspace/ModelsMetrics/by_id.csv"
+    df_by_id = metric_eval.save_function_results(
+        csv_path=df_by_id_path_csv, func_name="by_id"
+    )
+
+    df_by_doc_type_path_csv = "workspace/ModelsMetrics/df_by_doc_type.csv"
+    metric_eval.save_function_results(
+        csv_path=df_by_doc_type_path_csv, func_name="by_doc_type", func_arg=df_by_id
+    )
+
+    df_by_doc_question_path_csv = "workspace/ModelsMetrics/df_by_doc_question.csv"
+    metric_eval.save_function_results(
+        csv_path=df_by_doc_type_path_csv, func_name="by_doc_question", func_arg=df_by_id
+    )
+
+    df_general_csv_path = "workspace/ModelsMetrics/df_general.csv"
+    df_general = metric_eval.save_function_results(
+        csv_path=df_general_csv_path, func_name="general"
+    )
